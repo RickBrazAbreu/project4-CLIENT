4d977cd93cfaa71ea23fb90562aa38d3ebfca9c3
Pedro Abreu <henriqueibtou@gmail.com>
2022-08-27 19:48:26 -0700

    cart goin forward

--- src/components/views/Cart.js
+++ src/components/views/Cart.js
@@ -57,7 +57,7 @@ const SignIn = (props) => {
     return (
         <div className='row'>
             <div className='col-sm-10 col-md-8 mx-auto mt-5'>
-                <h3>Sign In</h3>
+                <h3>Here will be the cart,...</h3>
                 
             </div>
         </div>
