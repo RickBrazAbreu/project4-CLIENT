22f49103054976b792c9aa184b1138dd036bf4a3
Pedro Abreu <henriqueibtou@gmail.com>
2022-08-29 11:24:48 -0700

    initiial committts

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: '<https://git.heroku.com/yellow-stone-proj-back.git>',
+	production: 'https://git.heroku.com/yellow-stone-proj-back.git',
 	development: 'http://localhost:8200',
 }
 
